ccff51b31661c8e6298bd2a4ff6c2bd327e4992f
naresh <medaknaresh66@gmail.com>
2026-02-03 18:15:53 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1="Modi"
+PERSON2="Musk"
+
+
+echo "$PERSON1 :: Hello,$PERSON2 how are you?" 
+echo "$PERSON2  :: I'm good, $PERSON1 thank you! And you?"
+echo "$PERSON1 :: I'm doing well, thanks for asking."
+echo "$PERSON2  :: Great to hear! What are your plans for today?"
+echo "$PERSON1 :: I have a few meetings lined up. How about you?"
+echo "$PERSON2  :: I'm working on some new projects. Let's catch up later!"
