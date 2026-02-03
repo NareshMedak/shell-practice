8048acb38343de13fc90492172b137bb1a747f8e
naresh <medaknaresh66@gmail.com>
2026-02-03 19:36:45 +0530

    hello world started

--- 06-data -types.sh
+++ 06-data -types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NUMBER1=Naresh
+NUMBER1=100
 NUMBER2=200
 
 TIMESTAMP=$(date)
---
+++ 07-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MOVIES=("COURT" "HIT3" "PUSHPA" "THANDEL")
+
+echo "First Movie: $MOVIES[0]"
+
