a6b1de0ee1b255d50eb50eb0a524ed954e230603
naresh <medaknaresh66@gmail.com>
2026-02-03 18:05:20 +0530

    hello world started

---
+++ 02-conversationa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Trump :: Hello, how are you?" 
+echo "Musk  :: I'm good, thank you! And you?"
+echo "Trump :: I'm doing well, thanks for asking."
+echo "Musk  :: Great to hear! What are your plans for today?"
+echo "Trump :: I have a few meetings lined up. How about you?"
+echo "Musk  :: I'm working on some new projects. Let's catch up later!"
