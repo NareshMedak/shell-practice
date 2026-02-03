35e825d06ce5426aa2403afaea599dbf40fc7e22
naresh <medaknaresh66@gmail.com>
2026-02-03 18:33:58 +0530

    hello world started

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,6 @@
 PERSON1=$1
 PERSON2=$2
 
-
 echo "$PERSON1 :: Hello,$PERSON2 how are you?" 
 echo "$PERSON2  :: I'm good, $PERSON1 thank you! And you?"
 echo "$PERSON1 :: I'm doing well, thanks for asking."
---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter your pin number:"
+
+read PIN  #there is PIN variable 
+
+echo "Your number is $PIN"
