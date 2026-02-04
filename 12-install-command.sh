964807d326447fd990858e02d802f061b1a9d4c5
naresh <medaknaresh66@gmail.com>
2026-02-04 14:04:56 +0530

    special vars

--- 12-install-command.sh
+++ 12-install-command.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -eq 0]
 then 
     echo "ERROR:Please run this script as root user"
+    exit 1 #give other than 0 upto 127
 else 
     echo "you are running this with root access"
     
