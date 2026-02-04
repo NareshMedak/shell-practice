9f3c99692c17364719291a816eeabe282a1eceeb
naresh <medaknaresh66@gmail.com>
2026-02-04 14:23:30 +0530

    special vars

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER=$1 
 
--gt --> greater than
--lt --> less than
--eq --> equal to
--ne --> not equal to
+#-gt --> greater than
+#-lt --> less than
+#-eq --> equal to
+#-ne --> not equal to
 
 
 if [ $NUMBER -lt 50 ]
