a8338a48bbf9dd9dc13bd10c8c4ab76ec0796d57
naresh <medaknaresh66@gmail.com>
2026-02-04 12:57:41 +0530

    special vars

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -12,6 +12,7 @@ if [ $NUMBER -gt 10 ]
 then 
     echo "Given number $NUMBER is greater than 10"
 else 
-   echo "Given number $NUMBER is less than or equal to 10"
+   echo "Given number $NUMBER is less than or equal
+ to 10"
 
     fi 
