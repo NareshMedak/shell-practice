b51bc8f81da156bd2ba0c4d87c13ca428b8f82d0
naresh <medaknaresh66@gmail.com>
2026-02-17 18:26:31 +0530

    colors

--- 20-loops.sh
+++ 20-loops.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/bash
 
 USERID=$(id -u)
 R="\e[31m"
@@ -8,7 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "python3" "nginx" "httpd")
+PACKAGES=("mysql" "python" "nginx" "httpd")
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
@@ -32,15 +32,16 @@ VALIDATE(){
     fi
 }
 
-for packages in ${PACKAGES[@]}
+#for package in ${PACKAGES[@]}
+for package in $@
 do
-dnf list installed $package &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    echo "$package is not installed... going to install it" | tee -a $LOG_FILE
-    dnf install $package -y &>>$LOG_FILE
-    VALIDATE $? "$package"
-else
-    echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
-fi
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed... going to install it" | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
+    fi
 done
