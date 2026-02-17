0e6f3eff19ba8d0e8cd4e35d5f4e0a4401362e3d
naresh <medaknaresh66@gmail.com>
2026-02-17 16:15:44 +0530

    colors

--- 18-logs.sh
+++ 18-logs.sh
@@ -7,17 +7,17 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="var/log/shellscript-logs"
 SCRIPT_NAME="$(echo $0 |cut -d ":"-f1)"
-LOG_FLODER="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script started executing at: $(date)" &>>$LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo "$R ERROR:: Please run this script with root access $N"
+    echo "$R ERROR:: Please run this script with root access $N" &>>$LOG_FILE
     exit 1 #give other than 0 upto 127
 else
-    echo "You are running with root access"
+    echo "You are running with root access" &>>$LOG_FOLDER
 fi
 
 # validate functions takes input as exit status, what command they tried to install
